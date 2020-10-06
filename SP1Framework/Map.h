849f9d40232741f7285b8d0cd91e9a609ff846c5
SAMUEL PECK CHENG HAI <samuelpeck200@gmail.com>
2020-10-06 15:36:14 +0800

    Collusion

---
+++ SP1Framework/Map.h
@@ -0,0 +1,21 @@
+#ifndef MAP_H
+#define MAP_H
+#include <string>
+using namespace std;
+
+class Map
+{
+
+public:
+
+	int x;
+	int y;
+	Map();
+
+
+	unsigned short mapArray[80][25];
+	//void data(int x, int y);
+
+};
+
+#endif
